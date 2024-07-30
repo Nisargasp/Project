71354a2da5752844ad7795c31e99c6e5027a3e1f
Nisargasp <nisargasp84@gmail.com>
2024-07-30 23:50:58 +0530

    add files

---
+++ Wrapper_classes/Coil.java
@@ -0,0 +1,12 @@
+class Coil
+{
+	String color;
+	int price1;
+	
+	public Coil(String colorLocal,int price1Local)
+	{
+		color=colorLocal;
+		price1=price1Local;
+		System.out.println("the Coil least is created:");
+	}
+}
---
+++ Wrapper_classes/Led.java
@@ -0,0 +1,14 @@
+class Led
+{
+	String color;
+	int price1;
+	String brand;
+	
+    Led(String colorLocal,int price1Local,String brandLocal)
+	{
+		color=colorLocal;
+		price1=price1Local;
+		brand=brandLocal;
+		System.out.println("the Led least is created:");
+	}
+}
